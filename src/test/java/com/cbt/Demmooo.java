d028d30a022806f48aeafe113af3e652c05c0174
lyazzat2592 <kipshakbayevalyazzat@gmail.com>
2016-12-11 14:27:12 -0500

    initial commit again

---
+++ src/test/java/com/cbt/Demmooo.java
@@ -0,0 +1,7 @@
+package com.cbt;
+
+public class Demmooo {
+	public static void main(String[] args) {
+		System.out.println("hhellooooo");
+}
+}
--- src/test/java/com/cbt/DemoTest.java
+++ src/test/java/com/cbt/DemoTest.java
@@ -4,5 +4,6 @@ public class DemoTest {
 public static void main(String[] args) {
 	System.out.println("heloo world");
 	System.out.println("hello again");
+	
 }
 }
